7ca685e4388355c468786bf96a6c43fb1e3f013f
santiago <santiago@santiago-Inspiron-3583>
2020-08-21 14:59:05 -0500

    paginas web

---
+++ src/java/ChartUtilities.java
@@ -0,0 +1,20 @@
+
+import java.io.File;
+
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ *
+ * @author santiago
+ */
+class ChartUtilities {
+
+    static void saveChartAsPNG(File chartFile, JFreeChart jfreechart, int i, int i0) {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+    
+}
